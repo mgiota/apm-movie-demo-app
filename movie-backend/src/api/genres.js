7df3fdafd898ae57ff1aca1f249d3116511cbcb5
simitt <silvia.mitter@elastic.co>
2021-10-13 21:06:29 +0200

    Fix bug with next(err)

--- movie-backend/src/api/genres.js
+++ movie-backend/src/api/genres.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const db = require("../db");
 
-router.get("/", async (request, response) => {
+router.get("/", async (request, response, next) => {
   try {
     let genres = await db.fetchGenre();
     return response.json(genres.map(g => g.name))
